2efbb42538899bb0be71d0d59f02766edf51500a
Muhammadazim <odiljonov150@gmail.com>
2023-07-08 09:18:00 -0700

    first commit

---
+++ lib/screens/sing_up_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingUpScreen extends StatefulWidget {
+  const SingUpScreen({super.key});
+
+  @override
+  State<SingUpScreen> createState() => _SingUpScreenState();
+}
+
+class _SingUpScreenState extends State<SingUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
